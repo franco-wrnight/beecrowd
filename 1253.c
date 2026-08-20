7374d58b24ccf6d10396259c3bf7f9900cb60bee
Franco González <franco.gonza2116@gmail.com>
2026-08-20 12:48:12 -0400

    nuevos beecrowd

---
+++ 1253.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXLEN 55
+
+void cipherFoo(char *s, int shift)
+{
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        
+    }
+}
+
+int main(void)
+{
+    int n = 0;
+    scanf("%d", &n);
+    while (n--)
+    {
+        char s[MAXLEN];
+        int shift;
+
+        scanf("%s", s);
+        scanf("%d", &shift);
+
+        cipherFoo(s, shift);
+        printf("%s\n", s);
+    }
+    return 0;
+}
